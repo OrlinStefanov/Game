c1345add811a15d2410a879f16de467b6461c6b6
OrlinStefanov <orlinss2008@gmail.com>
2023-01-14 23:05:06 +0200

    kon

---
+++ Assets/Scenes/PlayerScripts/playerMove.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float speed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultipayer;
+    bool readyToJump = true;
+
+    [Header("Keybinds")]
+    public KeyCode jumpkey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatisground;
+    bool grounded;
+
+    public Transform orientation;
+
+    float hor;
+    float vert;
+
+    [Header("Animator")]
+    public Animator animator;
+
+    Vector3 moveDir;
+
+    Rigidbody rb;
+
+    private int num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatisground);
+
+        MyInput();
+        SpeedControll();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+            rb.mass = 1;
+        }
+        else
+        {
+            rb.drag = 0f;
+            rb.mass = 2;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Punch();
+        } else
+        {
+            CheckAnimations();
+        }
+
+        CheckSprint();
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        hor = Input.GetAxisRaw("Horizontal");
+        vert = Input.GetAxisRaw("Vertical");
+
+        //jump
+        if (Input.GetKey(jumpkey) && readyToJump)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+
+            animator.Play("JumpAll");
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDir = orientation.forward * vert + orientation.right * hor;
+
+        if (grounded)
+        {
+            rb.AddForce(moveDir.normalized * speed * 10f, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDir.normalized * speed * 10f * airMultipayer, ForceMode.Force);
+        }
+    }
+
+    private void SpeedControll()
+    {
+        Vector3 vector3 = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (vector3.magnitude > speed)
+        {
+            Vector3 limitedVel = vector3.normalized * speed;
+
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void CheckSprint()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 10f;
+            }
+            else
+            {
+                speed = 8f;
+            }
+        }
+    }
+
+    private void CheckAnimations()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.Play("RunForward");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.Play("RunBackward");
+        }
+    }
+
+    private void Punch()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            num++;
+
+            switch (num)
+            {
+                case 1:
+                    animator.Play("PunchLeft");
+                    break;
+
+                case 2:
+                    animator.Play("PunchRight");
+                    num = 0;
+                    break;
+            }
+        } 
+    }
+}
